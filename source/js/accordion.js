4c2155f827b81c9afb91da2090fd508bd82bfd45
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2021-10-21 19:16:10 +0300

    Added an animation for the accordion

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,12 +1,24 @@
 function initAccordion() {
   const accordionItems = document.querySelectorAll('.product__delivery-term');
+  const accordionItemTexts = document.querySelectorAll('.product__delivery-text');
+  const accordionItemTextHeights = [];
 
-  for (const item of accordionItems) {
-    item.addEventListener('click', function(evt) {
+  for (let i = 0; i < accordionItems.length; i++) {
+    accordionItemTexts[i].style.height = 'auto';
+    accordionItemTextHeights.push(accordionItemTexts[i].offsetHeight);
+    accordionItemTexts[i].style.removeProperty('height');
+
+    accordionItems[i].addEventListener('click', function(evt) {
       evt.preventDefault();
 
       if (evt.target.closest('.product__delivery-button')) {
         this.classList.toggle('product__delivery-term--opened');
+
+        if (accordionItemTexts[i].style.height) {
+          accordionItemTexts[i].style.removeProperty('height');
+        } else {
+          accordionItemTexts[i].style.height = `${accordionItemTextHeights[i]}px`;
+        }
       }
     });
   }
